fdf31410fb185fe2d94a7a6f8c7433725980f2dd
kastor <47624693+qkrtjsrbs315@users.noreply.github.com>
2023-03-16 14:38:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -49,27 +49,22 @@ def today_message(msg):
         return error_msg
 
 
-# discord Client class를 생성합니다.
 client = discord.Client(intents=intents)
 
-# event decorator를 설정하고 on_ready function을 할당해줍니다.
+
 @client.event
-async def on_ready():  # on_ready event는 discord bot이 discord에 정상적으로 접속했을 때 실행됩니다.
+async def on_ready():  
     print('We have logged in as {}'.format(client))
-    print('Bot name: {}'.format(client.user.name))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
-    print('Bot ID: {}'.format(client.user.id))  # 여기서 client.user는 discord bot을 의미합니다. (제가 아닙니다.)
+    print('Bot name: {}'.format(client.user.name)) 
+    print('Bot ID: {}'.format(client.user.id))  
 
 
-# event decorator를 설정하고 on_message function을 할당해줍니다.
+
 @client.event
 async def on_message(message):
-    # message란 discord 채널에 올라오는 모든 message를 의미합니다.
-    # 따라서 bot이 보낸 message도 포함이되죠.
-    # 아래 조건은 message의 author가 bot(=clinet.user)이라면 그냥 return으로 무시하라는 뜻입니다.
     if message.author == client.user:
         return
 
-    # message를 보낸 사람이 bot이 아니라면 message가 give로 시작하는 경우 채널에 급식이라는 글자를 보내라는 뜻입니다.
     elif message.content.startswith('!give_me_haksik'):
         info = today_message('give_me_haksik')
         await message.channel.send(info) #여기에 코드 박기
@@ -81,5 +76,4 @@ async def on_message(message):
         await message.channel.send(info) #여기에 코드 박기
 
 
-# 위에서 설정한 client class를 token으로 인증하여 실행합니다.
 client.run(discord_token)
